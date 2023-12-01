5369348803ece317944227ccbf85e9324dd087be
acttcha <acttcha@gmail.com>
2023-12-02 01:43:33 +0900

    리빈 작업 기능 일부 진행 및 inputList 추가

--- routes/rebin.js
+++ routes/rebin.js
@@ -24,4 +24,55 @@ router.get('/rebin1', (req, res) => {
   }
 })
 
+router.get('/workbench-check/:id', (req, res) => {
+  const { id } = req.params;
+
+  const query = 'SELECT * FROM workbench WHERE id = ?';
+
+  db.query(query, [id], (err, results) => {
+      if (err) {
+          console.error('데이터 검색 오류: ' + err.message);
+          res.json({ success: false, message: '데이터 검색 실패' });
+      } else {
+          if (results.length > 0) {
+              if (results[0].work_category == '리빈' && results[0].status == 1) {
+                  res.json({ success: true, message: '작업 가능한 리빈작업대 확인' });
+              } else if (results[0].work_category == '리빈' && results[0].status == 0) {
+                  res.json({ success: false, message: '현재 작업중인 리빈작업대 입니다.' });
+              } else if (results[0].work_category !== '리빈' && results[0].status == 1) {
+                  res.json({ success: false, message: '리빈 작업대가 아닙니다.' });
+              } else {
+                  res.json({ success: false, message: '값이 올바르지 않습니다.' });
+              }
+          } else {
+              res.json({ success: false, message: '일치하는 작업대가 없습니다.' });
+          }
+      }
+  });
+});
+
+router.get('/rebin2', (req, res) => {
+
+  if (req.user) {
+    if (req.user.is_admin === 0) {
+      if (req.user.work_access == '리빈' || req.user.work_access === 'ALL') {
+        const workbenchId = req.query.workbenchId;
+        res.render('work_rebin2.ejs', { user: req.user, workbenchId: workbenchId });
+      }
+      else {
+        res.status(500).send('작업 권한이 없습니다.');
+      }
+    }
+    else if (req.user.is_admin === 1) {
+      res.status(500).send('작업자로 로그인하세요.');
+    }
+    else {
+      res.redirect('/default')
+    }
+  }
+  else {
+    res.redirect('/login');
+  }
+})
+
 module.exports = router;
