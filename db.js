1b12c0b63703cf2c253cac784101f86b93deb80f
acttcha <acttcha@gmail.com>
2023-12-05 19:55:17 +0900

    GCP에 데이터베이스 호스팅 및 연결 설정(1차배포)

--- db.js
+++ db.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
+// 로컬 db 연결 코드
+// const db = mysql.createConnection({
+//   host: '127.0.0.1',
+//   port: 3306,
+//   user: 'user',
+//   password: process.env.DB_PASSWORD,
+//   database: 'fulfillment'
+// });
+
+
+// gcp db 연결 코드
 const db = mysql.createConnection({
-  host: 'localhost',
-  port: "3306",
+  host: '34.22.67.125',
+  port: 3306,
   user: 'user',
   password: process.env.DB_PASSWORD,
   database: 'fulfillment'
 });
 
 module.exports = db; 
+
